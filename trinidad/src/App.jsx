f4340cc726c9eb937d996d314b8b969690e289e6
Andero <andero.sc@gmail.com>
2025-02-18 20:37:09 +0200

    started trial project

---
+++ trinidad/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { Route, Routes } from "react-router"
+import Article from './pages/Article'
+import Intro from './pages/Intro'
+import Life from './pages/Life'
+import List from './pages/List'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/article" element={ <Article /> }/>
+        <Route path="/intro" element={ <Intro /> }/>
+        <Route path="/life" element={ <Life /> }/>
+        <Route path="/list" element={ <List /> }/>
+
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
